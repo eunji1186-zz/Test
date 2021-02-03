746fb1817626471e49b5cdd4ee7e41cf4a2a139b
eunji1186 <eunji1186@kakao.com>
2021-02-03 17:32:45 +0900

    fifth

--- App.js
+++ App.js
@@ -1,17 +1,129 @@
+//App.js
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput,TouchableOpacity, ScrollView, ImageBackground} from 'react-native';
+import Setting from './Setting.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class App extends React.Component {
-  
+
     constructor(props){
         super(props);
 
         this.state={
             dday: new Date(),
-            ddayTitle: '개강',
+            ddayTitle: '',
+            chatInput:'',
             catLog:[],
+            settingModal: false,
         }
     }
+
+    async UNSAFE_componentWillMount(){
+        try{
+            const ddayString = await AsyncStorage.getItem('@dday');
+            const chatLogString = await AsyncStorage.getItem('@chat');
+
+            if(chatLogString == null){
+                this.setState({chatLog:[]});
+            }
+
+            else{
+                const chatLog = JSON.parse(chatLogString);
+                this.setState({chatLog: chatLog});
+            }
+
+            if(ddayString ==null){
+                this.setState({
+                    dday: new Date(),
+                    ddayTitle: '',
+                });
+            }
+
+            else{
+                const dday=JSON.parse(ddayString);
+
+                this.setState({
+                    dday:new Date(dday.date),
+                    ddayTitle: dday.title,
+                });
+            }
+        }
+
+        catch(e){
+            console.log("ERR", e);
+        }
+    }
+
+    toggleSettingModal(){
+        this.setState({
+            settingModal: !this.state.settingModal
+        })
+    }
+
+    settingHandler(title, date){
+        this.setState({
+            ddayTitle: title;
+            dday: date,
+        });
+
+        this.toggleSettingModal();
+    }
+
+    makeDataString(){
+        return this.state.dday.getFullYear()+'년'+(this.state.dday.getMonth()+1)+'월'+this.state.dday.getDate()+'일';
+    }
+
+    makeRemainString() {
+        const distance = new Date().getTime() - this.state.dday.getTime();
+        console.log(new Date(), this.state.dday,distance / (1000 * 60 * 60 * 24) )
+        const remain = Math.floor(distance / (1000 * 60 * 60 * 24));
+        if(remain < 0) {
+          return 'D'+remain;
+        }
+
+        else if (remain > 0) {
+          return 'D+'+remain;
+        }
+
+        else if (remain === 0) {
+          return 'D-day';
+        }
+    }
+
+    chatHandler(){
+        this.setState({
+           chatLog: [...this.state.chatLog, this.makeDateString()+':'+this.state.chatInput],
+           chatInput:'',
+        }, async()=>{
+           const chatLogString = JSON.stringify(this.state.chatLog);
+           await AsyncStorage.setItem('@chat', chatLogString);
+        });
+    }
+
+    async settingHandler(title, date){
+        this.setState({
+            ddayTitle: title,
+            dday: date,
+        });
+
+        try{
+            const dday={
+                title: title,
+                date: date,
+            }
+
+            const ddayString = JSON.stringify(dday);
+            await AsyncStorage.setItem('@dday',ddayString);
+        }
+
+        catch(e){
+            console.log(e);
+        }
+
+        this.toggleSettingModal();
+    }
+
+
 
     render() {
     return (
@@ -21,7 +133,7 @@ export default class App extends React.Component {
             source={require('./image/background.png')}>
 
         <View style={styles.settingView}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.toggleSettingModal()}>
                 <Image source={require('./icon/setting.png')}/>
             </TouchableOpacity>
         </View>
@@ -32,29 +144,48 @@ export default class App extends React.Component {
             </Text>
 
             <Text style={styles.ddayText}>
-                D-27
+                {this.makeRemainString()}
             </Text>
 
             <Text style={styles.dateText}>
-                2021.02.03
+                {this.makeDataString()}
             </Text>
 
         </View>
 
         <View style={styles.chatView}>
             <ScrollView style={styles.chatScrollView}>
+
+                {this.state.chatLog.map((chat)=>{
+                    return <Text style={styles.chat}> {chat} </Text>
+                })}
             </ScrollView>
 
             <View style={styles.chatControl}>
-                <TextInput style={styles.chatInput}/>
+                <TextInput
+                    style={styles.chatInput}
+                    value={this.state.chatInput}
+                    onChangeText={(changedText)=>{
+                        console.log(this.state.chatLog)
+                        this.setState({chatInput: changedText})}
+                    }
+                />
 
-                 <TouchableOpacity style={styles.sendButton}>
+                 <TouchableOpacity
+                    style={styles.sendButton}
+                    onPress={()=>this.chatHandler()}
+                 >
                     <Text>
                          전송
                     </Text>
                 </TouchableOpacity>
             </View>
         </View>
+            {this.state.settingModal ?
+                <Setting
+                    modalHandler={()=>this.toggleSettingModal()}
+                    settingHandler={(title, date)=>this.settingHandler(title, date)}/>
+                : <></>}
       </ImageBackground>
       </View>
     );
@@ -64,6 +195,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+    },
+
+    chat:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+        margin: 2,
     },
 
     settingView:{
