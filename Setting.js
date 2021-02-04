a740fe91fa67d46dd4302097cc2fc5bc864e6df5
eunji1186 <eunji1186@kakao.com>
2021-02-04 11:21:50 +0900

    6th

--- Setting.js
+++ Setting.js
@@ -15,34 +15,41 @@ export default class Setting extends React.Component {
 
   render() {
     return (
-      <View  style={styles.container}>
-          <TouchableOpacity
-            style={styles.background}
-            activeOpacity={1}
-            onPress={this.props.modalHandler/>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.background}
+          activeOpacity={1}
+          onPress={this.props.modalHandler>
+          
           <View style={styles.modal}>
-            <Text style={styles.titleText}>설정</Text>
+            <Text style={styles.titleText}>
+              설정
+            </Text>
+
             <TextInput
-                style={styles.ddayInput}
-                value={this.stat.title}
-                onChangeText={(changedText)=>{this.setState({title: changedText})}}
-                palceholder={"디데이 제목을 입력해주세요."}/>
+              style={styles.ddayInput}
+              value={this.stat.title}
+              onChangeText={(changedText)=>{this.setState({title: changedText})}}
+              palceholder={"디데이 제목을 입력해주세요."}
+            />
 
             <DataPicker
-                data={this.stat.data}
-                onDataChange={(date)=>{this.setState({date: date})}}
-                mode='date'/>
+              data={this.stat.data}
+              onDataChange={(date)=>{this.setState({date: date})}}
+              mode='date'/>
 
-            <TouchableOpacity onPress={()=>this.props.settingHandler(this.state.title, this.state.date)}>
-              <Text style={styles.doneText}>
-                완료
-              </Text>
-            </TouchableOpacity>
-          </View>
+           <TouchableOpacity
+            onPress={()=>this.props.settingHandler(this.state.title, this.state.date)}
+           >
+
+           <Text style={styles.doneText}>
+              완료
+           </Text>
+           </TouchableOpacity>
         </View>
-      );
-    }
-  }
+      </View>
+    );
+   }
 
 const styles = StyleSheet.create({
   container: {
@@ -51,12 +58,14 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: 'transparent'
   },
+
   background: {
     position: 'absolute',
     height: '100%',
     width: '100%',
     backgroundColor: 'rgba(0,0,0,0.5)'
   },
+
   ddayInput: {
     backgroundColor: 'white',
     marginBottom: 20,
@@ -65,6 +74,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#a5a5a5'
   },
+
   modal: {
     marginHorizontal: 20,
     borderRadius: 10,
@@ -72,13 +82,15 @@ const styles = StyleSheet.create({
     marginTop: '50%',
     backgroundColor: 'white',
   },
+
   doneText: {
     color: 'rgb(1,123,255)',
     fontSize: 15,
     margin: 10
   },
+
   titleText: {
     fontSize: 18,
     margin: 10
-  }
+  },
 });
